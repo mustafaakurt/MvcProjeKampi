72421176b3f0640475629cb4234bce2732868ab6
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-02-21 15:57:20 +0300

    category-about-contact entity classları eklendi

---
+++ EntitiyLayer/Concrete/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyLayer.Concrete
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public String AboutDetails1 { get; set; }
+        public String AboutDetails2 { get; set; }
+        public String AboutImage1 { get; set; }
+        public String AboutImage2 { get; set; }
+    }
+}
