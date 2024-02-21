67cabb26a7dc6f182465e3ce6d6964398ab8697a
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-02-21 15:47:23 +0300

    heading-content-writer entity classları eklendi

---
+++ BusinessLayer/Concrete/Content.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class Content
+    {
+        public int ContentID { get; set; }
+        public String ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+        // ContentWriter
+        // ContentTitle
+
+    }
+}
