01bba4917d4bcae5666bc35bfba6e445a33c913f
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-02-22 18:47:33 +0300

    ICategoryService eklendi

--- 01-MvcProjeKampi/Controllers/CategoryController.cs
+++ 01-MvcProjeKampi/Controllers/CategoryController.cs
@@ -19,8 +19,8 @@ namespace _01_MvcProjeKampi.Controllers
         }
         public ActionResult GetCategoryList()
         {
-            var categoryvalues = cm.GetAllBL();
-            return View(categoryvalues);
+            //var categoryvalues = cm.GetAllBL();
+            return View();
         }
 
         [HttpGet]
@@ -32,7 +32,7 @@ namespace _01_MvcProjeKampi.Controllers
         [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-            cm.CategoryAddBL(p);
+            //cm.CategoryAddBL(p);
             return RedirectToAction("GetCategoryList");
         }
     }
